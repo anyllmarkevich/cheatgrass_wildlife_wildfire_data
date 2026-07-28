68fe481472f29b15e20b8f3f7439dd69e849d361
Anyll Markevich <anyllmarkevich@gmail.com>
2026-07-27 20:18:29 -0600

    Added a script for managing vegetation data.

---
+++ R/01_vegetation_survey_processing.R
@@ -0,0 +1,75 @@
+# ===========================================================  
+# Script: 01_vegetation_survey_processing.R  
+# Author: Anyll Markevich
+# Date: 2026-07-27  
+# Description: Process raw vegetation survey data into usable formats
+# =========================================================== 
+
+# 1. PACKAGES
+library(here)
+
+# 2. LOAD DATA
+data_2020 <- read.csv(here::here("data", "raw", "vegetation_survey_2020.csv"))
+data_2021 <- read.csv(here::here("data", "raw", "vegetation_survey_2021.csv"))
+
+# Update column names to snake case for consistency
+colnames(data_2020) <- c("plot", "direction", "distance", "column", "row", "content_id")
+colnames(data_2021) <- c("plot", "direction", "distance", "column", "row", "content_id")
+
+# 3. DEFINITIONS
+# Define all possible vegetation categories (order matters)
+veg_categories <- c("cheatgrass_plant", "cheatgrass_litter", "bare_ground", "other_litter", "other_plant")
+
+# Convert a table of how many times each ID occurs into a vector that contains the count of all IDs, matching the columns of a summary table
+table_to_row <- function(table) {
+  row <- rep(0, 5)
+  for (content in names(table)) {
+    row[as.integer(content)] <- table[[content]]
+  }
+  return(as.vector(row))
+}
+
+# 4. COMBINE DATA
+# Prepare to merge 2020 and 2021 data by adding a year column to each data set
+data_2020 <- cbind(year = rep(2020, nrow(data_2020)), data_2020)
+data_2021 <- cbind(year = rep(2021, nrow(data_2021)), data_2021)
+
+# Combine the 2020 and 2021 into a single sorted data set to make further analysis easier
+point_data <- rbind(data_2020, data_2021)
+point_data <- point_data[order(point_data$year, point_data$plot, point_data$direction, point_data$distance, point_data$column, point_data$row),]
+
+# 5. PROCESS DATA
+# Summarize the contents of each sampling frame by counting the number of each content ID within a frame
+sample_data <- data.frame(matrix(ncol = 9, nrow = 0))
+colnames(sample_data) <- c("year", "plot", "direction", "distance", veg_categories)
+for (this_year in unique(point_data$year)) {
+  for (this_plot in unique(point_data$plot)) {
+    for (this_direction in unique(point_data$direction)) {
+      for (this_distance in unique(point_data$distance)) {
+        sample <- point_data |> 
+          subset(year == this_year & plot == this_plot & direction == this_direction & distance == this_distance, select = "content_id") |> table() |> 
+          table_to_row()
+          sample_data[nrow(sample_data) + 1,] <- c(this_year, this_plot, this_direction, this_distance, sample)
+      }
+    }
+  }
+}
+
+# Summarize the contents of each plot by finding the proportion of each vegetation type on each plot
+plot_data <- data.frame(matrix(ncol = 7, nrow = 0))
+colnames(plot_data) <- c("year", "plot", veg_categories)
+for (this_year in unique(sample_data$year)){
+  for (this_plot in unique(sample_data$plot)) {
+    proportions <- subset(sample_data, plot==this_plot & year == this_year, select = veg_categories) |> colSums() |> proportions()
+    plot_data[nrow(plot_data) + 1,] <- c(this_year, this_plot, proportions)
+  }
+}
+
+# 6. SAVE OUTPUTS
+# Save formatted vegetation data for every sampling point (a single pin within a sampling quadrant)
+write.csv(here::here(point_data, "data", "processed", "vegetation_point_data.csv"))
+# Save formatted summary of vegetation data for every sampling frame (a whole sampling quadrant)
+write.csv(here::here(sample_data, "data", "processed", "vegetation_pampling_frame_data.csv"))
+# Save formatted summary of vegetation data for every plot
+write.csv(here::here(plot_data, "data", "processed", "vegetation_plot_data.csv"))
+
