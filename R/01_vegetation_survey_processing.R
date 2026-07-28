110703e0332020958a6f4f157645deec005d4a92
Anyll Markevich <anyllmarkevich@gmail.com>
2026-07-27 21:50:52 -0600

    Formatting changes.

--- R/01_vegetation_survey_processing.R
+++ R/01_vegetation_survey_processing.R
@@ -1,9 +1,9 @@
-# ===========================================================  
-# Script: 01_vegetation_survey_processing.R  
+# ===========================================================
+# Script: 01_vegetation_survey_processing.R
 # Author: Anyll Markevich
-# Date: 2026-07-27  
+# Date: 2026-07-27
 # Description: Process raw vegetation survey data into usable formats
-# =========================================================== 
+# ===========================================================
 
 # 1. PACKAGES
 library(here)
@@ -28,7 +28,7 @@ table_to_row <- function(table) {
     content_num <- which(veg_categories == content)
     row[as.integer(content_num)] <- table[[content]]
   }
-  return(as.vector(row))
+  as.vector(row)
 }
 
 # 4. COMBINE DATA
@@ -38,7 +38,7 @@ data_2021 <- cbind(year = rep(2021, nrow(data_2021)), data_2021)
 
 # Combine the 2020 and 2021 into a single sorted data set to make further analysis easier
 point_data <- rbind(data_2020, data_2021)
-point_data <- point_data[order(point_data$year, point_data$plot, point_data$direction, point_data$distance, point_data$column, point_data$row),]
+point_data <- point_data[order(point_data$year, point_data$plot, point_data$direction, point_data$distance, point_data$column, point_data$row), ]
 
 # 5. PROCESS DATA
 # Convert numerical vegetation categories into readable categories
@@ -52,10 +52,11 @@ for (this_year in unique(point_data$year)) {
   for (this_plot in unique(point_data$plot)) {
     for (this_direction in unique(point_data$direction)) {
       for (this_distance in unique(point_data$distance)) {
-        sample <- point_data |> 
-          subset(year == this_year & plot == this_plot & direction == this_direction & distance == this_distance, select = "content") |> table() |> 
+        sample <- point_data |>
+          subset(year == this_year & plot == this_plot & direction == this_direction & distance == this_distance, select = "content") |>
+          table() |>
           table_to_row()
-          sample_data[nrow(sample_data) + 1,] <- c(this_year, this_plot, this_direction, this_distance, sample)
+        sample_data[nrow(sample_data) + 1, ] <- c(this_year, this_plot, this_direction, this_distance, sample)
       }
     }
   }
@@ -64,10 +65,12 @@ for (this_year in unique(point_data$year)) {
 # Summarize the contents of each plot by finding the proportion of each vegetation type on each plot
 plot_data <- data.frame(matrix(ncol = 7, nrow = 0))
 colnames(plot_data) <- c("year", "plot", veg_categories)
-for (this_year in unique(sample_data$year)){
+for (this_year in unique(sample_data$year)) {
   for (this_plot in unique(sample_data$plot)) {
-    proportions <- subset(sample_data, plot==this_plot & year == this_year, select = veg_categories) |> colSums() |> proportions()
-    plot_data[nrow(plot_data) + 1,] <- c(this_year, this_plot, proportions)
+    proportions <- subset(sample_data, plot == this_plot & year == this_year, select = veg_categories) |>
+      colSums() |>
+      proportions()
+    plot_data[nrow(plot_data) + 1, ] <- c(this_year, this_plot, proportions)
   }
 }
 
@@ -78,4 +81,3 @@ write.csv(point_data, here::here("data", "processed", "vegetation_point_data.csv
 write.csv(sample_data, here::here("data", "processed", "vegetation_pampling_frame_data.csv"), row.names = FALSE)
 # Save formatted summary of vegetation data for every plot
 write.csv(plot_data, here::here("data", "processed", "vegetation_plot_data.csv"), row.names = FALSE)
-
